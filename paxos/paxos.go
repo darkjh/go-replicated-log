2386e3a43f05c9e81fe6e2125a03b9583e101c70
Han JU <ju.han.felix@gmail.com>
2014-07-01 23:09:41 +0200

    paxos: add pseudo-code in comments

--- paxos/paxos.go
+++ paxos/paxos.go
@@ -19,6 +19,39 @@ package paxos
 // px.Max() int -- highest instance seq known, or -1
 // px.Min() int -- instances before this seq have been forgotten
 //
+//
+// Pseudo-code for a single paxos instance:
+//
+// proposer(v):
+//   while not decided:
+//     choose n, unique and higher than any n seen so far
+//     send prepare(n) to all servers including self
+//     if prepare_ok(n_a, v_a) from majority:
+//       v' = v_a with highest n_a; choose own v otherwise
+//       send accept(n, v') to all
+//       if accept_ok(n) from majority:
+//         send decided(v') to all
+//
+// acceptor's state:
+//   n_p (highest prepare seen)
+//   n_a, v_a (highest accept seen)
+//
+// acceptor's prepare(n) handler:
+//   if n > n_p
+//     n_p = n
+//     reply prepare_ok(n_a, v_a)
+//   else
+//     reply prepare_reject
+//
+// acceptor's accept(n, v) handler:
+//   if n >= n_p
+//     n_p = n
+//     n_a = n
+//     v_a = v
+//     reply accept_ok(n)
+//   else
+//     reply accept_reject
+//
 
 import "net"
 import "net/rpc"
