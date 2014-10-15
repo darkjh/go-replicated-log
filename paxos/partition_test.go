a81f15776ecc4955791612655c4bb99ce0bc621a
Han JU <ju.han.felix@gmail.com>
2014-10-15 22:39:06 +0200

    test: adapt chaos test (random partition/instance/unreliable)

--- paxos/partition_test.go
+++ paxos/partition_test.go
@@ -4,6 +4,7 @@ import "testing"
 import "runtime"
 import "fmt"
 import "time"
+import "math/rand"
 
 func TestPartition(t *testing.T) {
 	runtime.GOMAXPROCS(4)
@@ -132,6 +133,110 @@ func TestChangingPartitionsUnreliable(t *testing.T) {
 		}
 
 		waitn(t, pxa, seq, 5)
+	}
+
+	fmt.Printf("  ... Passed\n")
+}
+
+func TestChaos(t *testing.T) {
+	runtime.GOMAXPROCS(4)
+
+	fmt.Printf("Test: Many requests, changing partitions ...\n")
+
+	const npaxos = 5
+	var pxa []*Paxos = make([]*Paxos, npaxos)
+	var pxh []string = make([]string, npaxos)
+	defer cleanup(pxa)
+
+	for i := 0; i < npaxos; i++ {
+		pxh[i] = port(i)
+		pxa[i] = Make(pxh, i, nil)
+		pxa[i].unreliable = true
+	}
+	defer heal()
+
+	done := false
+
+	// re-partition periodically
+	ch1 := make(chan bool)
+	go func() {
+		defer func() {
+			ch1 <- true
+		}()
+		for done == false {
+			var a [npaxos]int
+			for i := 0; i < npaxos; i++ {
+				a[i] = (rand.Int() % 3)
+			}
+			pa := make([][]int, 3)
+			for i := 0; i < 3; i++ {
+				pa[i] = make([]int, 0)
+				for j := 0; j < npaxos; j++ {
+					if a[j] == i {
+						pa[i] = append(pa[i], j)
+					}
+				}
+			}
+			partition(t, pa[0], pa[1], pa[2])
+			time.Sleep(time.Duration(rand.Int63()%200) * time.Millisecond)
+		}
+	}()
+
+	seq := 0
+
+	// periodically start a new instance
+	ch2 := make(chan bool)
+	go func() {
+		defer func() {
+			ch2 <- true
+		}()
+		for done == false {
+			// how many instances are in progress?
+			nd := 0
+			for i := 0; i < seq; i++ {
+				if ndecided(t, pxa, i) == npaxos {
+					nd++
+				}
+			}
+			if seq-nd < 10 {
+				for i := 0; i < npaxos; i++ {
+					pxa[i].Start(seq, rand.Int()%10)
+				}
+				seq++
+			}
+			time.Sleep(time.Duration(rand.Int63()%300) * time.Millisecond)
+		}
+	}()
+
+	// periodically check that decisions are consistent
+	ch3 := make(chan bool)
+	go func() {
+		defer func() {
+			ch3 <- true
+		}()
+		for done == false {
+			for i := 0; i < seq; i++ {
+				ndecided(t, pxa, i)
+			}
+			time.Sleep(time.Duration(rand.Int63()%300) * time.Millisecond)
+		}
+	}()
+
+	time.Sleep(20 * time.Second)
+	done = true
+	<-ch1
+	<-ch2
+	<-ch3
+
+	// repair, then check that all instances decided.
+	for i := 0; i < npaxos; i++ {
+		pxa[i].unreliable = false
+	}
+	heal()
+	time.Sleep(5 * time.Second)
+
+	for i := 0; i < seq; i++ {
+		waitmajority(t, pxa, i)
 	}
 
 	fmt.Printf("  ... Passed\n")
